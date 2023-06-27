5b6b415f5baebc7017fb2dd8c84a62e91c87135f
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-06-26 23:21:10 -0300

    config global types and user entity

---
+++ src/@types/global.d.ts
@@ -0,0 +1,40 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    PORT: int;
+  }
+}
+
+declare interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  cpf: string;
+  password: string;
+  password_confirmation?: string;
+  role: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+declare interface IRequestUser {
+  name: string;
+  email: string;
+  cpf: string;
+  password: string;
+  password_confirmation?: string;
+  role: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+declare interface HTTPResponseObject {
+  withError?: boolean
+  data?: object
+  status?: number
+  error?: string
+}
+
+declare interface HTTPRequestObject {
+  id?: string
+  user?: IUser
+}
---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,33 @@
+
+export class UserEntity implements IUser {
+  constructor({
+    id,
+    name,
+    email,
+    cpf,
+    password,
+    password_confirmation,
+    role,
+    createdAt,
+    updatedAt,
+  }: IUser) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.cpf = cpf;
+    this.password = password;
+    this.password_confirmation = password_confirmation;
+    this.role = role;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+  id: string;
+  name: string;
+  email: string;
+  cpf: string;
+  password: string;
+  password_confirmation?: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
